7feaef7c5e10b35bbb1ea9092fecedc0d7bc803e
Amrendra <amrendranathme0016@gmail.com>
2022-05-26 21:19:47 +0530

    Initial Commit

---
+++ Data_preparation.py
@@ -0,0 +1,4 @@
+from pyspark.sql import SparkSession
+spark=SparkSession.builder.master("local").appName("test").getOrCreate()
+sc=spark.sparkContext
+df=spark.read.csv("file:///H:\SampleData\\100Employees_record.csv",header=True)
